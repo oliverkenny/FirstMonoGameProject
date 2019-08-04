96072bf23dd9918101d9fae107d867ef1e161aa6
oliverkenny <oliver.j.kenny@gmail.com>
2019-08-04 13:28:33 +0100

    Add project files.

---
+++ FirstMonoGameProject/Ball.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstMonoGameProject
+{
+	public class Ball
+	{
+		private Vector2 Position { get; set; }
+		private Vector2 Velocity { get; set; }
+		private string TextureName { get; set; }
+		private Texture2D Texture { get; set; }
+		private float MaxSpeed { get; set; }
+		private float Acceleration { get; set; }
+		private float JumpHeight { get; set; }
+		private float BounceRate { get; set; }
+
+		public Ball(
+			Vector2? position = null,
+			Vector2? velocity = null,
+			string texture = "ball",
+			float? maxSpeed = null,
+			float? acceleration = null,
+			float? jumpHeight = null,
+			float? bounceRate = null,
+			GraphicsDeviceManager graphics = null
+		)
+		{
+			this.Position = position ?? new Vector2(0, 0);
+			this.Velocity = velocity ?? new Vector2(0, 0);
+			this.TextureName = texture;
+			this.MaxSpeed = maxSpeed ?? 50f;
+			this.Acceleration = acceleration ?? 50f;
+			this.JumpHeight = jumpHeight ?? 50f;
+			this.BounceRate = bounceRate ?? 1.3f;
+			if (graphics != null)
+				this.Position = new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
+		}
+
+		public void Up(GameTime gameTime)
+		{
+			Velocity = new Vector2(this.Velocity.X, this.Velocity.Y - (this.Acceleration * (float)gameTime.ElapsedGameTime.TotalSeconds));
+		}
+
+		public void Down(GameTime gameTime)
+		{
+			Velocity = new Vector2(this.Velocity.X, this.Velocity.Y + (this.Acceleration * (float)gameTime.ElapsedGameTime.TotalSeconds));
+		}
+
+		public void Left(GameTime gameTime)
+		{
+			Velocity = new Vector2(this.Velocity.X - (this.Acceleration * (float)gameTime.ElapsedGameTime.TotalSeconds), this.Velocity.Y);
+		}
+
+		public void Right(GameTime gameTime)
+		{
+			Velocity = new Vector2(this.Velocity.X + (this.Acceleration * (float)gameTime.ElapsedGameTime.TotalSeconds), this.Velocity.Y);
+		}
+
+		public void LoadTexture(ContentManager content)
+		{
+			this.Texture = content.Load<Texture2D>(this.TextureName);
+		}
+
+		public void Update(GameTime gameTime, GraphicsDeviceManager graphics)
+		{
+			// Update Velocity
+			if (Velocity.Y > 0)
+				Velocity = new Vector2(this.Velocity.X, this.Velocity.Y - ((Acceleration / 2) * (float)gameTime.ElapsedGameTime.TotalSeconds));
+			if (Velocity.Y < 0)
+				Velocity = new Vector2(this.Velocity.X, this.Velocity.Y + ((Acceleration / 2) * (float)gameTime.ElapsedGameTime.TotalSeconds));
+			if (Velocity.X > 0)
+				Velocity = new Vector2(this.Velocity.X - ((Acceleration / 2) * (float)gameTime.ElapsedGameTime.TotalSeconds), this.Velocity.Y);
+			if (Velocity.X < 0)
+				Velocity = new Vector2(this.Velocity.X + ((Acceleration / 2) * (float)gameTime.ElapsedGameTime.TotalSeconds), this.Velocity.Y);
+
+
+			// Update Position
+			this.Position = new Vector2(this.Position.X + this.Velocity.X, this.Position.Y + this.Velocity.Y);
+
+			// Restrict Velocity
+			if (this.Position.X <= this.Texture.Width / 2 || this.Position.X >= graphics.PreferredBackBufferWidth - (this.Texture.Width / 2))
+				this.Velocity = new Vector2(-this.Velocity.X / this.BounceRate, this.Velocity.Y);
+			if (this.Position.Y <= this.Texture.Height / 2 || this.Position.Y >= graphics.PreferredBackBufferHeight - (this.Texture.Height / 2))
+				this.Velocity = new Vector2(this.Velocity.X, -this.Velocity.Y / this.BounceRate);
+
+			// Restrict Position
+			this.Position =
+				new Vector2(
+					Math.Min(
+						Math.Max(this.Texture.Width / 2, this.Position.X),
+						graphics.PreferredBackBufferWidth - this.Texture.Width / 2
+					),
+					Math.Min(
+						Math.Max(this.Texture.Height / 2, this.Position.Y),
+						graphics.PreferredBackBufferHeight - this.Texture.Height / 2
+					)
+				);
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			spriteBatch.Draw(this.Texture, this.Position, null, Color.White, 0f, new Vector2(this.Texture.Width / 2, this.Texture.Height / 2), Vector2.One, SpriteEffects.None, 0f);
+		}
+	}
+}
---
+++ FirstMonoGameProject/Game1.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace FirstMonoGameProject
+{
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Game
+	{
+		const int DISPLAY_W = 1920;
+		const int DISPLAY_H = 1080;
+		List<Ball> Balls = new List<Ball>();
+
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			graphics.PreferredBackBufferWidth = DISPLAY_W;
+			graphics.PreferredBackBufferHeight = DISPLAY_H;
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			//// TODO: Add your initialization logic here
+
+			Random rand = new Random();
+			var ballCount = 100;
+			for (int i = 0; i < ballCount; i++)
+			{
+				Balls.Add(
+					new Ball(
+						position:
+							new Vector2(
+								rand.Next(50, graphics.PreferredBackBufferWidth - 50),
+								rand.Next(50, graphics.PreferredBackBufferHeight - 50)
+							),
+						acceleration: rand.Next(50, 200),
+						maxSpeed: rand.Next(50, 100),
+						texture: rand.Next(2) == 1 ? "ball" : "ball2",
+						bounceRate: 0.9f
+						)
+					);
+			}
+
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			// TODO: use this.Content to load your game content here
+			Balls.ForEach(x => x.LoadTexture(Content));
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// game-specific content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+			// TODO: Unload any non ContentManager content here
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			// TODO: Add your update logic here
+			var kState = Keyboard.GetState();
+
+			// Acceleration
+			if (kState.IsKeyDown(Keys.Up))
+				Balls.ForEach(x => x.Up(gameTime));
+			if (kState.IsKeyDown(Keys.Down))
+				Balls.ForEach(x => x.Down(gameTime));
+			if (kState.IsKeyDown(Keys.Left))
+				Balls.ForEach(x => x.Left(gameTime));
+			if (kState.IsKeyDown(Keys.Right))
+				Balls.ForEach(x => x.Right(gameTime));
+
+			Balls.ForEach(x => x.Update(gameTime, graphics));
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			// TODO: Add your drawing code here
+			spriteBatch.Begin();
+			Balls.ForEach(x => x.Draw(spriteBatch));
+			spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
